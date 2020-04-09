202253f56b57bfc83ddcc9f341fc882e5676bed6
wuli-erqio <1766458200@qq.com>
2020-04-09 09:28:48 +0800

    物品的增删改查

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
 import CategoryCreate from '../views/CategoryCreate.vue'
 import CategoryList from '../views/CategoryList.vue'
+import ItemCreate from '../views/ItemCreate.vue'
+import ItemList from '../views/ItemList.vue'
 
 Vue.use(VueRouter)
 
@@ -14,7 +16,11 @@ const routes = [
     children: [
       {path: '/categories/create', component: CategoryCreate },
       {path: '/categories/edit/:id', component: CategoryCreate, props:true },
-      {path: '/categories/list', component: CategoryList}
+      {path: '/categories/list', component: CategoryList},
+
+      {path: '/items/create', component: ItemCreate },
+      {path: '/items/edit/:id', component: ItemCreate, props:true },
+      {path: '/items/list', component: ItemList}
     ]
   }
 ]
---
+++ admin/src/views/ItemList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>物品列表</h1>
+    <el-table
+      :data="items"
+      style="width: 100%"
+    >
+      <el-table-column prop="_id" label="ID" width="300"></el-table-column>
+      <el-table-column prop="name" label="物品名称"> </el-table-column>
+      <el-table-column prop="icon" label="物品图标"> </el-table-column>
+      <el-table-column label="操作" width="100">
+      <template slot-scope="scope">
+        <el-button type="text" size="small" @click="$router.push(`/items/edit/${scope.row._id}`)">编辑</el-button>
+        <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  created() {
+    this.fetch()
+  },
+  methods: {
+    async fetch () {
+      const res = await this.$http.get('rest/items')
+      this.items = res.data
+    },
+    async remove(row) {
+      this.$confirm(`是否确定删除分类 "${row.name}"`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const res = await this.$http.delete(`rest/items/${row._id}`)
+        console.log(res)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+        this.fetch()
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.el-table .warning-row {
+  background: oldlace;
+}
+
+.el-table .success-row {
+  background: #f0f9eb;
+}
+</style>
