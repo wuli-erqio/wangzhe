7347667f33a2677b4c42e0e8888609b865d7c389
wuli-erqio <1766458200@qq.com>
2020-05-01 23:37:48 +0800

    首页新闻资讯-数据接口

--- server/models/Article.js
+++ server/models/Article.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-  title: {
-    type: String
-  },
-  categories: [{
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'Category'
-  }],
-  body: { type: String }
+  categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
+  title: { type: String },
+  body: { type: String },
 }, {
   timestamps: true
 })
