859655c891e23a4782cbe115a856575da5648cad
wuli-erqio <1766458200@qq.com>
2020-05-04 21:44:04 +0800

    首页英雄列表-页面展示

--- web/src/components/ListCard.vue
+++ web/src/components/ListCard.vue
@@ -1,37 +1,38 @@
 <template>
   <m-card :icon="icon" :title="title">
     <div class="nav jc-between">
-      <div class="nav-item" :class="{active: active === i}" v-for="(category, i) in categories" :key="i">
-        <div class="nav-link" @click="active = $refs.list.$swiper.slideTo(i)">{{category.name}}</div>
+      <div class="nav-item" :class="{active: active === i}"
+      v-for="(category, i) in categories" :key="i"
+      @click="$refs.list.$swiper.slideTo(i)">
+        <div class="nav-link">{{category.name}}</div>
       </div>
     </div>
-    <swiper
-      class="pt-3"
-      ref="list"
-      @slide-change="() => active = $refs.list.$swiper.realIndex" >
-      <swiper-slide v-for="(category, i) in categories" :key="i">
-        <!-- 此处为插槽，具名插槽，绑定传给父元素 -->
-        <slot name="items" :category="category"></slot>
-      </swiper-slide>
-    </swiper>
+    <div class="pt-3">
+      <swiper ref="list" :options="{autoHeight: true}"
+      @slide-change="() => active = $refs.list.$swiper.realIndex">
+        <swiper-slide v-for="(category, i) in categories" :key="i">
+          <!-- 此处为插槽，具名插槽，绑定传给父元素 -->
+          <slot name="items" :category="category"></slot>
+        </swiper-slide>
+      </swiper>
+    </div>
   </m-card>
 </template>
 
 <script>
 export default {
   props: {
-    title: { type: String, required: true },
     icon: { type: String, required: true },
-    categories: { type: Array, required: true}
+    title: { type: String, required: true },
+    categories: { type: Array, required: true }
   },
-  data() {
+  data(){
     return {
       active: 0
     }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -32,7 +32,16 @@
     </m-list-card>
     <!-- end of news -->
 
-    <m-card icon="card-hero" title="英雄列表"></m-card>
+    <m-list-card icon="card-hero" title="英雄列表" :categories="heroCats">
+      <template #items="{category}">
+        <div class="d-flex flex-wrap" style="margin: 0 -0.5rem;">
+          <div class="p-2 text-center" style="width: 20%" v-for="(hero, i) in category.heroList" :key="i">
+            <img :src="hero.avatar" class="w-100" alt=""/>
+            <div>{{hero.name}}</div>
+          </div>
+        </div>
+      </template>
+    </m-list-card>
     <!-- end of hero -->
 
     <m-card icon="shipin" title="精彩视频"></m-card>
@@ -58,17 +67,23 @@ export default {
           el: '.pagination-home'
         }
       },
-      newsCats: []
+      newsCats: [],
+      heroCats: []
     }
   },
   created() {
     this.fetchNewsCats()
+    this.fetchHeroCats()
   },
   methods: {
     async fetchNewsCats() {
       const res = await this.$http.get('news/list')
       this.newsCats = res.data
-      console.log(res)
+    },
+    async fetchHeroCats() {
+      const res = await this.$http.get('heroes/list')
+      this.heroCats = res.data
+      console.log(res.data)
     }
   }
 }
