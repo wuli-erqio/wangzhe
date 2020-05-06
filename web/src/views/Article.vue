def5de595982cc0494f8e5ee6408f0b5af01676d
wuli-erqio <1766458200@qq.com>
2020-05-06 18:03:10 +0800

    新闻详情页-完善

--- web/src/views/Article.vue
+++ web/src/views/Article.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="page-article" v-if="model">
-    <div class="d-flex py-3 px-1">
-      <div class="iconfont icon-Back"></div>
-      <strong class="flex-1">
+    <div class="d-flex py-3 px-2 border-bottom">
+      <div class="iconfont icon-Back text-blue"></div>
+      <strong class="flex-1 text-blue pl-2">
         {{model.title}}
       </strong>
       <div class="text-grey fs-xs">
         2019-06-19
       </div>
     </div>
-    <div v-html="model.body"></div>
+    <div v-html="model.body" class="px-3 body fs-lg"></div>
+    <div class="px-3 border-top py-3">
+      <div class="d-flex ai-center">
+        <i class="iconfont icon-menu"></i>
+        <strong class="text-blue fs-lg ml-1">相关资讯</strong>
+      </div>
+      <div class="pt-2 fs-lg">
+        <router-link
+          class="py-1"
+          :to="`/articles/${item._id}`"
+          tag="div"
+          v-for="item in model.related"
+          :key="item._id">
+          {{item.title}}</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,6 +41,13 @@ export default {
   created() {
     this.fetch()
   },
+  watch: {
+    // 简写
+    id: 'fetch'
+    // id() {
+    //   this.fetch()
+    // }
+  },
   methods: {
     async fetch() {
       const res = await this.$http.get(`articles/${this.id}`)
@@ -35,6 +57,20 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.page-article {
+  .icon-Back {
+    font-size: 1.5rem;
+  }
+  .body {
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+    iframe {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
 </style>
